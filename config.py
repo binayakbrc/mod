a171a0da4b006ce1de9957b85c26dc615fe3cd59
Binayak Roy Chowdhury <64516592+binayakbrc@users.noreply.github.com>
2024-09-27 18:49:34 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,17 @@
+# Don't use quotes( " and ' )
+#SCRIPT BY SharpX72
+  
+#Enter Your Bot Token here get it from @botfarher
+BOT_TOKEN=("7755448178:AAFjscxaVAa01S7Kru8-bnRZ8A2c_hwB03Q")
+
+  #Enter Your telegram username here without @
+OWNER_USERNAME=("NSG_Rajesh")
+
+  #Enter your admin id here Get it from @missRose_bot by typing /info
+ADMIN_IDS=("625938207")
+
+
+
+
+
+  
